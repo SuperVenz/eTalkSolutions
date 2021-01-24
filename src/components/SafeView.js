22696d55d5f81ecb805a5fe369a4c565657bd3fe
Alec Venzor <alecvenzordev@gmail.com>
2021-01-23 17:57:37 -0800

    fixed text

--- src/components/SafeView.js
+++ src/components/SafeView.js
@@ -32,7 +32,7 @@ function SafeView() {
     <Wrapper>
       <InfoView
         title="Safe and Secure for your users. Peace of mind for you."
-        text="While speed, performance, and design are what we all like to think about when it comes to building the best sites on the internet, security is most often overlooked. Malicious users on the web steal personal information, bring down sites, and exploit fundamental security flaws in web servers. all Solution tackles this problem by using new serverless technology to deliver your websites. No server means no reachable database for malicious requests, DDOS attacks, or accidental exposure.  The security of your digital footprint is something we don’t take for granted."
+        text="Speed, performance, and design are what we all like to think about when it comes to building the best sites on the internet, security is most often overlooked. Malicious users on the web steal personal information, bring down sites, and exploit fundamental security flaws in web servers. all Solution tackles this problem by using new serverless technology to deliver your websites. No server means no reachable database for malicious requests, DDOS attacks, or accidental exposure.  The security of your digital footprint is something we don’t take for granted."
       />
       <MedContainer>
         <Img fluid={data.file.childImageSharp.fluid} alt="tag" />
