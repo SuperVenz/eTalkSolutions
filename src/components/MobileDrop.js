8d8696860898cdccf6205f7248f5b33185e72261
Alec Venzor <alecvenzordev@gmail.com>
2021-02-02 15:53:51 -0800

    mobile speed

--- src/components/MobileDrop.js
+++ src/components/MobileDrop.js
@@ -65,6 +65,7 @@ const MobileDrop = () => {
         {data.allNavJson.nodes.map((nav) => (
           <AniLink
             cover
+            duration={0.5}
             bg="
     url(https://images.unsplash.com/photo-1612153018787-4899c6e056d7?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=675&q=80)
     center / cover   /* position / size */
